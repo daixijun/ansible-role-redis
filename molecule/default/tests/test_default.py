163d29535d6586792c4d027e3d009ea71e0136eb
Xijun Dai <daixijun1990@gmail.com>
2019-07-09 00:37:24 +0800

    重构

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,26 +1,37 @@
 import os
 import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
 
 
 def test_redis_installed(host):
-    package_name = _get_redis_package_name(host.system_info.distribution)
-    package = host.package(package_name)
+    f = host.file("/usr/local/bin/redis-server")
 
-    assert package.is_installed
-
-
-def _get_redis_package_name(host_distro):
-    return {
-        "centos": "redis",
-        "ubuntu": "redis-server",
-        "debian": "redis-server"
-    }.get(host_distro, "redis")
+    assert f.exists
+    assert f.is_file
 
 
-def test_redis_running(host):
-    service = host.service('redis_6379')
+def test_redis_user_and_group(host):
+    user = host.user("redis")
+    group = host.group("redis")
+
+    assert user.name == "redis"
+    assert user.shell == "/sbin/nologin"
+    assert user.home == "/var/lib/redis"
+    assert user.group == "redis"
+
+    assert group.exists
+
+
+def test_redis_6379_running_and_enabled(host):
+    service = host.service("redis_6379")
 
     assert service.is_running
+    assert service.is_enabled
+
+
+def test_redis_6380_running_and_enabled(host):
+    service = host.service("redis_6380")
+
+    assert service.is_running
+    assert service.is_enabled
